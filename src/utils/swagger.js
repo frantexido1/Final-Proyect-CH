269692470e67172d5ff0a3e614a1122d22f6f872
Fran Texido <fran.texido@gmail.com>
2023-12-17 22:31:50 -0300

    actualizacion swagger

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -3,14 +3,15 @@ const swaggerOptions = {
     openapi: "3.0.0",
     info: {
       title: "APICoder",
-      version: "1.0.0",
-      description: "API",
+      version: "1.1.0",
+      description: "Ecommerce CoderHouse Francisco-Texido",
     },
   },
   apis: [
     "./src/docs/users/users.yaml",
     "./src/docs/products/products.yaml",
     "./src/docs/cart/cart.yaml",
+    "./src/docs/sessions/sessions.yaml",
   ],
 };
 
